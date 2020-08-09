aa9328261e9466b7c3d5b0490b691f62b95482ad
pablos16 <pablos16@ucm.es>
2020-08-09 12:24:38 +0200

    Configuration with XML file instead of annotations

--- src/main/java/es/springframework/chucknorrisjokesforactuator/ChuckNorrisJokesForActuatorApplication.java
+++ src/main/java/es/springframework/chucknorrisjokesforactuator/ChuckNorrisJokesForActuatorApplication.java
@@ -2,12 +2,13 @@ package es.springframework.chucknorrisjokesforactuator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:jokes-config.xml")
 public class ChuckNorrisJokesForActuatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChuckNorrisJokesForActuatorApplication.class, args);
     }
-
 }
